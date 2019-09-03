39c5a46c28629bfe95438d6f4af5b1ede556850e
AmateurECE <edtwardy@mtu.edu>
2019-09-02 22:44:41 -0400

    StandardFilesystemAdaptor: Implementation
    This commit implements most of the functionality for the Standard
    Filesystem Adaptor (Untested). Unfortunately, due to the nature of
    this class, it cannot be tested using Unit Tests. There will likely
    be some Verification tests, potentially using some Shell glue logic
    for automation.

--- include/SortMedia/Adaptors/StandardFilesystemAdaptor.h
+++ include/SortMedia/Adaptors/StandardFilesystemAdaptor.h
@@ -25,27 +25,17 @@ public:
   virtual std::iterator_traits<Path> walk(const Path& directory)
     const final override;
 
-  // TODO: Test isEmpty
   virtual bool isEmpty(const Path& p) const final override;
-  // TODO: Test isDirectory
   virtual bool isDirectory(const Path& p) const final override;
 
-  // TODO: Test rename
   virtual void rename(const Path& oldPath, const Path& newPath)
     const final override;
-  // TODO: Test remove
-  virtual bool remove(const Path& p) const final override;
-  // TODO: Test equivalent
+  virtual void remove(const Path& p) const final override;
   virtual bool equivalent(const Path& p1, const Path& p2) const final override;
-  // TODO: Test exists
   virtual bool exists(const Path& p) const final override;
-  // TODO: Test currentPath
   virtual Path currentPath() const final override;
-  // TODO: Test createDirectory
-  virtual bool createDirectory(const Path& p) const final override;
-  // TODO: Test createDirectories
-  virtual bool createDirectories(const Path& p) const final override;
-  // TODO: Test absolute
+  virtual void createDirectory(const Path& p) const final override;
+  virtual void createDirectories(const Path& p) const final override;
   virtual Path absolute(const Path& p) const final override;
 };
 
--- include/SortMedia/Interfaces/IFilesystemAdaptor.h
+++ include/SortMedia/Interfaces/IFilesystemAdaptor.h
@@ -26,16 +26,26 @@ public:
   virtual ~IFilesystemAdaptor() {};
   virtual std::iterator_traits<Path> walk(const Path& directory) const = 0;
 
+  // Tests whether a given path points to an existing, empty file.
   virtual bool isEmpty(const Path& p) const                             = 0;
+  // Tests whether a given path points to an existing directory.
   virtual bool isDirectory(const Path& p) const                         = 0;
 
+  // Rename the file `oldPath' to the path pointed to by `newPath'
   virtual void rename(const Path& oldPath, const Path& newPath) const   = 0;
-  virtual bool remove(const Path& p) const                              = 0;
+  // Remove the file from the filesystem and delete its contents.
+  virtual void remove(const Path& p) const                              = 0;
+  // Test whether two paths are equivalent.
   virtual bool equivalent(const Path& p1, const Path& p2) const         = 0;
+  // Test whether the path `p' points to an existing location on the filesystem
   virtual bool exists(const Path& p) const                              = 0;
+  // Return the current path.
   virtual Path currentPath() const                                      = 0;
-  virtual bool createDirectory(const Path& p) const                     = 0;
-  virtual bool createDirectories(const Path& p) const                   = 0;
+  // Create a directory at the path pointed to by p.
+  virtual void createDirectory(const Path& p) const                     = 0;
+  // Create a directory at the path pointed to by p (and any intermediaries)
+  virtual void createDirectories(const Path& p) const                   = 0;
+  // If `p' starts with '.' or '..', expand to obtain the absolute path.
   virtual Path absolute(const Path& p) const                            = 0;
 };
 
