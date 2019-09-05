8d4fa3592734811a0ad249042591d969d5cc8009
AmateurECE <edtwardy@mtu.edu>
2019-09-05 16:41:46 -0400

    Patch AnyFileLocator into FileLocatorFactory

--- source/SortMedia/Factories/FileLocatorFactory.cpp
+++ source/SortMedia/Factories/FileLocatorFactory.cpp
@@ -7,21 +7,22 @@
 //
 // CREATED:         08/27/2019
 //
-// LAST EDITED:     08/27/2019
+// LAST EDITED:     09/05/2019
 ////
 
 #include <SortMedia/Factories/FileLocatorFactory.h>
 
+#include <SortMedia/FileLocators/AnyFileLocator.h>
+
 SortMedia::Factories::FileLocatorFactory
 ::FileLocatorFactory()
-{
-}
+{}
 
 std::unique_ptr<SortMedia::Interfaces::IFileLocator>
 SortMedia::Factories::FileLocatorFactory
 ::makeFileLocator() const
 {
-  return nullptr;
+  return std::make_unique<FileLocators::AnyFileLocator>();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
