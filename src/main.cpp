bcd8b9a7d910e44226bfaeb865c25e7568c02dce
Ethan D. Twardy <ethan.twardy@gmail.com>
2024-03-23 15:34:02 -0500

    Integrate CLI11 and begin developing command line interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
+#include <CLI/CLI.hpp>
 #include <iostream>
 
-int main() { std::cout << "Hello, world!\n"; }
+using namespace std;
+
+int main(int argc, char** argv) {
+    CLI::App app{"Media library sorting application"};
+
+    string input_tree = "", output_tree = "";
+    app.add_option("-i", input_tree, "Input file tree");
+    app.add_option("-o", output_tree, "Output location");
+
+    CLI11_PARSE(app, argc, argv);
+
+    std::cout << "input_tree=" << input_tree << ", output_tree=" << output_tree
+              << "\n";
+}
