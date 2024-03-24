56779a17ee7ea9979a7f2f76d21b9ec191169883
Ethan D. Twardy <ethan.twardy@gmail.com>
2024-03-24 17:39:44 -0500

    Create library file iterator

---
+++ src/library.h
@@ -0,0 +1,80 @@
+#include <filesystem>
+#include <regex>
+
+/// An iterator over media files in a library. This iterator fulfills the
+/// requirements of an input iterator, and is also a range type.
+class LibraryFileIterator {
+public:
+  using difference_type = std::ptrdiff_t;
+  using element_type = std::filesystem::directory_entry;
+  using pointer = const element_type*;
+  using reference = const element_type&;
+
+  LibraryFileIterator(std::regex file_filter, std::string directory)
+      : m_inner{std::filesystem::recursive_directory_iterator(directory)},
+        m_file_filter{file_filter} {
+    find_captured_element();
+  }
+
+  reference operator*() const { return *m_inner; }
+  LibraryFileIterator& operator++() {
+    ++m_inner;
+    find_captured_element();
+    return *this;
+  }
+  LibraryFileIterator operator++(int) {
+    LibraryFileIterator temp = *this;
+    ++(*this);
+    return temp;
+  }
+  bool operator==(const LibraryFileIterator& that) const {
+    return m_inner == that.m_inner;
+  }
+  bool operator!=(const LibraryFileIterator& that) const {
+    return !(m_inner == that.m_inner);
+  }
+
+  static LibraryFileIterator end() noexcept {
+    return LibraryFileIterator(
+        std::filesystem::end(std::filesystem::recursive_directory_iterator()));
+  }
+
+private:
+  std::filesystem::recursive_directory_iterator m_inner;
+  std::regex m_file_filter;
+
+  LibraryFileIterator(std::filesystem::recursive_directory_iterator inner)
+      : m_inner{inner}, m_file_filter{} {
+    find_captured_element();
+  }
+
+  void find_captured_element() {
+    const auto end{
+        std::filesystem::end(std::filesystem::recursive_directory_iterator())};
+    while (end != m_inner && !is_captured_element())
+      ++m_inner;
+  }
+
+  bool is_captured_element() const {
+    return m_inner->is_regular_file() &&
+           std::regex_match(m_inner->path().c_str(), m_file_filter);
+  }
+};
+
+static_assert(std::input_iterator<LibraryFileIterator>);
+
+/// A MusicLibrary is a tree of artifacts on disk that include music files,
+/// their associated metadata, cover art images, and directory structures
+/// that arbitrarily group music files together. The library yields an
+/// iterator over the media files in the library.
+class MusicLibrary {
+public:
+  MusicLibrary(std::string directory) : m_directory{directory} {}
+  LibraryFileIterator begin() {
+    return {std::regex(".*\\.flac$"), m_directory};
+  }
+  LibraryFileIterator end() { return LibraryFileIterator::end(); }
+
+private:
+  std::string m_directory;
+};
--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,27 @@
 #include <CLI/CLI.hpp>
 #include <iostream>
 
+#include "library.h"
 #include "version.h"
 
 using namespace std;
+using namespace std::filesystem;
 
 int main(int argc, char** argv) {
-    CLI::App app{"Media library sorting application"};
-    app.set_version_flag("--version", version::VERSION);
+  CLI::App app{"Media library sorting application"};
+  app.set_version_flag("--version", version::VERSION);
 
-    string input_tree = "", output_tree = "";
-    app.add_option("-i", input_tree, "Input file tree");
-    app.add_option("-o", output_tree, "Output location");
+  string input_tree{""}, output_directory{""};
+  app.add_option("-i", input_tree, "Input file tree");
+  app.add_option("-o", output_directory, "Output directory");
 
-    CLI11_PARSE(app, argc, argv);
+  CLI11_PARSE(app, argc, argv);
 
-    std::cout << "input_tree=" << input_tree << ", output_tree=" << output_tree
-              << "\n";
+  std::cout << "input_tree=" << input_tree
+            << ", output_tree=" << output_directory << "\n";
+
+  MusicLibrary library{input_tree};
+  for (const auto& entry : library) {
+    std::cout << entry << '\n';
+  }
 }
